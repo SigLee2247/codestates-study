aaa41f42dd31410ff1244dca54e1ab9535f6e6f8
LEEGIHO94 <lgh941107@gmail.com>
2022-12-05 16:25:36 +0900

    FINISH Project

--- src/main/java/com/codestates/migrations/schema.sql
+++ src/main/java/com/codestates/migrations/schema.sql
@@ -1,4 +1,3 @@
--- 여기 user 테이블 생성 SQL 구문이 있습니다. user 테이블을 참고해서, content 테이블 생성 SQL 구문을 완성하세요.
 
 CREATE TABLE `user` (
   `id` int PRIMARY KEY AUTO_INCREMENT,
@@ -7,26 +6,30 @@ CREATE TABLE `user` (
 );
 
 CREATE TABLE `content` (
-  -- TODO: `id`
-  -- TODO: `title`
-  -- TODO: `body`
+  `id` int PRIMARY KEY AUTO_INCREMENT,
+  `title` varchar(255) not NULL,
+  `body` varchar(255) not NULL,
   `created_at` timestamp not NULL DEFAULT CURRENT_TIMESTAMP,
   `userId` int,
   FOREIGN KEY (`userId`) REFERENCES `user` (`id`)
 );
 
--- PART 3:
--- 아래 주석을 제거하고 category, content_category, role 테이블을 만드세요.
 
 
---CREATE TABLE `category` (
---  -- TODO:
---);
---CREATE TABLE `content_category` (
---  -- TODO:
---);
---CREATE TABLE `role` (
---  -- TODO:
---);
---ALTER TABLE `user` ADD roleId int;
---ALTER TABLE `user` ADD FOREIGN KEY (`roleId`) REFERENCES `role` (`id`);
+CREATE TABLE `category` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
+    `name` varchar(255) not NULL
+);
+CREATE TABLE `content_category` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
+    `contentId` int not NULL ,
+    `categoryId` int not NULL,
+    FOREIGN KEY (`contentId`) REFERENCES `user` (`id`),
+    FOREIGN KEY (`categoryId`) REFERENCES `user` (`id`)
+);
+CREATE TABLE `role` (
+    `id` int PRIMARY KEY AUTO_INCREMENT,
+    `name` varchar(255)
+);
+ALTER TABLE `user` ADD roleId int;
+ALTER TABLE `user` ADD FOREIGN KEY (`roleId`) REFERENCES `role` (`id`);
