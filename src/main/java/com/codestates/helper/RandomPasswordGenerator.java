5c397c38458cb6c2f603bcc443a7b33fb02e7d93
LEEGIHO94 <lgh941107@gmail.com>
2023-01-03 17:48:56 +0900

    First

--- src/main/java/com/codestates/helper/RandomPasswordGenerator.java
+++ src/main/java/com/codestates/helper/RandomPasswordGenerator.java
@@ -8,17 +8,23 @@ import java.util.List;
 import static java.util.stream.Collectors.toList;
 
 public class RandomPasswordGenerator {
-    public static String generate(int numberOfUpperCaseLetters,
-                                  int numberOfLowerCaseLetters,
-                                  int numberOfNumeric,
-                                  int numberOfSpecialChars) {
+    public static String generate(int numberOfUpperCaseLetters,  //첫 인자 : 생성될 패스워드 문자열에서 알파벳 대문자의 개수
+                                  int numberOfLowerCaseLetters,  //두번쨰인자 : 생성될 패스워드 문자열에서 소문자 갯수
+                                  int numberOfNumeric,           //문자열에서 0 이상인 숫자의 갯수
+                                  int numberOfSpecialChars) {    // 특수문자의 갯수
+
         String upperCaseLetters = RandomStringUtils.random(numberOfUpperCaseLetters, 65, 90, true, false);
+
+        //start end는 각각 아스키테이블에 나와있는 문자의 값들을 의미한다.
+
         String lowerCaseLetters = RandomStringUtils.random(numberOfLowerCaseLetters, 97, 122, true, false);
         String numbers = RandomStringUtils.randomNumeric(numberOfNumeric);
         String specialChars = RandomStringUtils.random(numberOfSpecialChars, 33, 47, false, false);
 
         String combinedLetters = combineLetters(upperCaseLetters, lowerCaseLetters, numbers, specialChars);
+
         List<Character> shuffledLetters = shuffleLetters(combinedLetters);
+
         return shuffledLetters.stream()
                 .collect(StringBuilder::new, StringBuilder::append, StringBuilder::append)
                 .toString();
--- src/test/java/com/codestates/homework/StampCalculatorTest.java
+++ src/test/java/com/codestates/homework/StampCalculatorTest.java
@@ -1,18 +1,60 @@
 package com.codestates.homework;
 
+import com.codestates.helper.StampCalculator;
+import com.codestates.order.entity.Order;
+import com.codestates.order.entity.OrderCoffee;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class StampCalculatorTest {
     @Test
     @DisplayName("실습1: 스탬프 카운트 계산 단위 테스트")
     public void calculateStampCountTest() {
-        // TODO 여기에 테스트 케이스를 작성해주세요.
+        //given
+        int nowCount = 10;
+        int earned = 5;
+
+
+        //when
+        int actual = StampCalculator.calculateStampCount(nowCount, earned);
+        int expected = 15;
+
+
+        //then
+        assertEquals(actual, expected);
+        org.assertj.core.api.Assertions.assertThat(actual).isEqualTo(expected);
+
     }
 
     @Test
     @DisplayName("실습1: 주문 후 누적 스탬프 카운트 계산 단위 테스트")
     public void calculateEarnedStampCountTest(){
         // TODO 여기에 테스트 케이스를 작성해주세요.
+        //given
+        Order order = new Order();
+        OrderCoffee sample1 = new OrderCoffee();
+        sample1.setQuantity(10);
+        OrderCoffee sample2 = new OrderCoffee();
+        sample2.setQuantity(20);
+
+        order.setOrderCoffees(List.of(sample1, sample2));
+
+
+
+        //when
+
+        int actual = order.getOrderCoffees().stream().mapToInt(orderCoffee -> orderCoffee.getQuantity()).sum();
+        int expected = sample1.getQuantity() + sample2.getQuantity();
+
+
+
+        //then
+        assertEquals(actual,expected);
+
     }
 }
